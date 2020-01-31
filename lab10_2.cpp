d9d579022a9c90bf97cd6c7eaf6991e5717246bc
lab302-22 <lab302-22@labeng.local>
2020-01-31 17:54:27 +0700

    lab10_2 first work

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<string>
+#include<ctime>
+#include<cstdlib>
 
 using namespace std;
 
@@ -8,11 +10,15 @@ int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
 
 int drawCard(void){
 	//Write the function to random the number from 1 to 13 and return that random number.
+	int num = rand()%13 + 1;
+	return num;
 }
 
 int calScore(int x,int y,int z){
 	//Write the function to calculate the score with is the rightmost digit of summation of the scores from three cards.
 	//Return the score. 
+	int cal = (x+y+z)%10;
+	return cal;
 }
 
 int findYugiAction(int s){	
@@ -20,28 +26,40 @@ int findYugiAction(int s){
 	else if(s < 6) return 1; // Yugi will definitely draw (1) when current score (s) is less than 6
 	else{
 		// If current score is 6,7,8, Yugi will draw with probability 69% and will stay with probability 31% 
-		// Write conditions here using random number 
+		// Write conditions here using random number
+		srand(time(0));
+		int t = rand()%69+1;
+		if(t <= 69){
+			return 1;
+		}else{
+			return 2;
+		}
 	}
 }
 
 void checkWinner(int p, int y){
 	// Write condition for cout in this function
 	cout << "\n---------------------------------\n";
+	if(p == y){
 	cout <<   "|             Draw!!!           |"; // when p is equal to y
+	}if(p > y){
 	cout <<   "|         Player wins!!!        |"; // when p is greater than y
+	}if(p < y){
 	cout <<   "|          Yugi wins!!!         |"; // when p is less than y
+	}
 	cout << "\n---------------------------------\n";
 }
 
 int main(){	
+	srand(time(0));
 	int playerScore, yugiScore, playerAction, yugiAction;
-	int playerCards[3] = {drawCard(), drawCard(), 0};
-	int yugiCards[3]; //This line of code is not completed. You need to initialize value of yugiCards[].
+	int playerCards[3] = {drawCard(), drawCard(),drawCard()};
+	int yugiCards[3] = {drawCard(), drawCard(),drawCard()}; //This line of code is not completed. You need to initialize value of yugiCards[].
 	
 	
 	cout << "---------ORE NO TURN, DRAW!!!------------\n";
 	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
-	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+	playerScore = calScore(cardScores[playerCards[0]],cardScores[playerCards[1]],0);
 	cout << "Your score: " << playerScore << "\n";
 	do{
 		cout << "(1) Destiny draw (2) Stay, SELECT: ";
@@ -49,29 +67,29 @@ int main(){
 	}while(playerAction != 1 && playerAction != 2);
 	if(playerAction == 1){
 		//The following lines of code are not completed. Please correct it.
-		/*
 		cout << "Player draws the 3rd card!!!" << "\n";
-		cout << "Your 3rd card: " << "\n";
-		cout << "Your new score: " << "\n";
-		*/
+		cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
+		playerScore = calScore(cardScores[playerCards[0]],cardScores[playerCards[1]],cardScores[playerCards[2]]);
+		cout << "Your new score: " << playerScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n\n";
 	
 	
 	//The following lines of code for Yugi's turn are not completed. Please correct it.
-	/*
+
 	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
-	cout << "Yugi's cards: " << "\n";
-	yugiScore = ?????;
+	cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]]<< "\n";
+	yugiScore = calScore(cardScores[yugiCards[0]],cardScores[yugiCards[1]],0);
 	cout << "Yugi's score: " << yugiScore << "\n";
 	yugiAction = findYugiAction(yugiScore);
 	if(yugiAction == 1){
 		cout << "Yugi draws the 3rd card!!!\n";
-		cout << "Yugi's 3rd card: " << "\n";
-		cout << "Yugi's new score: " << "\n";
+		cout << "Yugi's 3rd card: " << cardNames[yugiCards[2]] <<"\n";
+		yugiScore = calScore(cardScores[yugiCards[0]],cardScores[yugiCards[1]],cardScores[yugiCards[2]]);
+		cout << "Yugi's new score: " << yugiScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n";
-	*/
+	
 	
 	checkWinner(playerScore,yugiScore);
 }
