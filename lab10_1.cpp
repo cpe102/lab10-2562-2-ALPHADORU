142f81ea856f4011ff061f2fffdc6e177315d13a
lab302-22 <lab302-22@labeng.local>
2020-01-31 17:53:58 +0700

    lab10_1 first work

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get(); // Wait for a user to press an enter key
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    string y;
+    if(x == 0) y = "A";
+    else if(x == 1) y = "B+";
+    else if(x == 2) y = "B";
+    else if(x == 3) y = "C+";
+    else if(x == 4) y = "C";
+    else if(x == 5) y = "D+";
+    else if(x == 6) y = "D";
+    else if(x == 7) y = "F";
+    else y = "W";
+    cout << "You will get "<< y <<" in this 261102.";
+}
